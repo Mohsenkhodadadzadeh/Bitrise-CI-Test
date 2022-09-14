4cecdf6a71de4ab8c778fd8104abca3d6f8a684d
Mohsen Khodadadzadeh <mohsen.khodadadzadeh@thelist-app.com>
2022-09-14 15:53:23 +0300

    initial project

---
+++ Bitrise CI test/Bitrise_CI_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bitrise_CI_testApp.swift
+//  Bitrise CI test
+//
+//  Created by Mohsen on 9/14/22.
+//
+
+import SwiftUI
+
+@main
+struct Bitrise_CI_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
